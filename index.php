1fb5e2273550e61bded3108286a5a5a43a0f895f
Olegs Osipovs <olegs.osipovs@gmail.com>
2019-02-14 22:02:54 +0200

    Navigation bar added

--- index.php
+++ index.php
@@ -17,74 +17,121 @@ else{
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	</head>
 	<body>
-		<div id="nowPlayingBarContainer">
-			<div id="nowPlayingBar">
-				
-				<div id="nowPlayingLeft">
-					<div class="content">
-						<span class="albumLink">
-							<img src="https://i.ytimg.com/vi/rb8Y38eilRM/hqdefault.jpg?v=4ffb4222" class="albumArtwork">
-						</span>
+		<div id="mainContainer">
+				<div id="topContainer">
 
-						<div class="trackInfo">
-							<span class="trackName">
-								<span>Happy Birthday!</span> 
-							</span>
+					<div id="navBarContainer">
+						<nav class="navBar">
+							<a href="index.php" class="logo">
+								<img src="assets/images/icons/pug.png" alt="logoIcon">
+							</a>
 
-							<span class="artistName">
-								<span>Olegs Osipovs</span> 
-							</span>
-
-						</div>
-
-					</div>
-				</div>
-				
-				<div id="nowPlayingCenter">
-					<div class="content playerControls">
-						<div class="buttons">
-
-							<button class="controlButton shuffle" title="Shuffle button">
-								<img src="assets/images/icons/shuffle.png" alt="Shuffle">
-							</button>
-
-							<button class="controlButton previous" title="Previous button">
-								<img src="assets/images/icons/previous.png" alt="Previous">
-							</button>
-
-							<button class="controlButton play" title="Play button">
-								<img src="assets/images/icons/play.png" alt="Play">
-							</button>
-
-							<button class="controlButton pause" title="Pause button" style="display: none;">
-								<img src="assets/images/icons/pause.png" alt="Pause">
-							</button>
-
-							<button class="controlButton next" title="Next button">
-								<img src="assets/images/icons/next.png" alt="Next">
-							</button>
-
-							<button class="controlButton repeat" title="Repeat button">
-								<img src="assets/images/icons/repeat.png" alt="Repeat">
-							</button>
-
-						</div>
-
-						<div class="playbackBar">
-							<span class="progressTime current">0.00</span>
-							<div class="progressBar">
-								<div class="progressBarBg">
-									<div clas="progress"></div>
+							<div class="group">
+								<div class="navItem">
+									<a href="search.php" class="navItemLink">Search
+										<img src="assets/images/icons/search.png" class="icon" alt="search">
+									</a>
 								</div>
 							</div>
-							<span class="progressTime remaining">0.00</span>
-							
-						</div>
+
+							<div class="group">
+								<div class="navItem">
+									<a href="browse.php" class="navItemLink">Browse</a>
+								</div>
+
+								<div class="navItem">
+									<a href="yourMusic.php" class="navItemLink">Your Music</a>
+								</div>
+								
+								<div class="navItem">
+									<a href="profile.php" class="navItemLink">Olegs Osipovs</a>
+								</div>
+
+							</div>
+						</nav>
 					</div>
 				</div>
 
-				<div id="nowPlayingRight">
+				<div id="nowPlayingBarContainer">
+
+				<div id="nowPlayingBar">
 					
+					<div id="nowPlayingLeft">
+						<div class="content">
+							<span class="albumLink">
+								<img src="https://i.ytimg.com/vi/rb8Y38eilRM/hqdefault.jpg?v=4ffb4222" class="albumArtwork">
+							</span>
+
+							<div class="trackInfo">
+								<span class="trackName">
+									<span>Happy Birthday!</span> 
+								</span>
+
+								<span class="artistName">
+									<span>Olegs Osipovs</span> 
+								</span>
+
+							</div>
+
+						</div>
+					</div>
+					
+					<div id="nowPlayingCenter">
+						<div class="content playerControls">
+							<div class="buttons">
+
+								<button class="controlButton shuffle" title="Shuffle button">
+									<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+								</button>
+
+								<button class="controlButton previous" title="Previous button">
+									<img src="assets/images/icons/previous.png" alt="Previous">
+								</button>
+
+								<button class="controlButton play" title="Play button">
+									<img src="assets/images/icons/play.png" alt="Play">
+								</button>
+
+								<button class="controlButton pause" title="Pause button" style="display: none;">
+									<img src="assets/images/icons/pause.png" alt="Pause">
+								</button>
+
+								<button class="controlButton next" title="Next button">
+									<img src="assets/images/icons/next.png" alt="Next">
+								</button>
+
+								<button class="controlButton repeat" title="Repeat button">
+									<img src="assets/images/icons/repeat.png" alt="Repeat">
+								</button>
+
+							</div>
+
+							<div class="playbackBar">
+								<span class="progressTime current">0.00</span>
+								<div class="progressBar">
+									<div class="progressBarBg">
+										<div clas="progress"></div>
+									</div>
+								</div>
+								<span class="progressTime remaining">0.00</span>
+								
+							</div>
+						</div>
+					</div>
+
+					<div id="nowPlayingRight">
+						<div class="volumeBar">
+							<button class="controlButton volume" title="Volume button">
+								<img src="assets/images/icons/volume.png" alt="Volume">
+							</button>
+
+							<div class="progressBar">
+									<div class="progressBarBg">
+										<div clas="progress"></div>
+									</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
