7ab3f73665dc06f391a68eb57e951f4bc29bdc15
Olegs Osipovs <olegs.osipovs@gmail.com>
2019-04-01 20:47:52 +0300

    Album details, beginning

---
+++ album.php
@@ -0,0 +1,23 @@
+ <?php 
+ include("includes/header.php"); 
+ include("includes/classes/Artist.php");
+ if(isset($_GET['id'])){
+ 	$albumId =  $_GET['id'];
+ }
+ else{
+ 	header("Location: index.php");
+ }
+
+ $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+ $album = mysqli_fetch_array($albumQuery);
+ 
+
+ $artist = new Artist($con, $album['artist']);
+
+
+ echo $artist->getName();
+
+ ?>
+
+
+<?php include("includes/footer.php"); ?>
